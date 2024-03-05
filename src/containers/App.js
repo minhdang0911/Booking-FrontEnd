7c93ff6016ee4cbdb8147c352d66ef6a93d49473
Minh Dang <minhdang9a8@gmail.com>
2024-03-05 12:57:50 +0700

    Desin Header HomePage

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,25 +2,24 @@ import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter as Router } from 'connected-react-router';
-import { history } from '../redux'
+import { history } from '../redux';
 import { ToastContainer } from 'react-toastify';
-
 
 import { userIsAuthenticated, userIsNotAuthenticated } from '../hoc/authentication';
 
-import { path } from '../utils'
+import { path } from '../utils';
 
 import Home from '../routes/Home';
 import Login from './Auth/login';
 // import Login from '../routes/Login';
 import Header from './Header/Header';
 import System from '../routes/System';
+import HomePage from './HomePage/HomePage.js';
 
 import { CustomToastCloseButton } from '../components/CustomToast';
 import ConfirmModal from '../components/ConfirmModal';
 
 class App extends Component {
-
     handlePersistorState = () => {
         const { persistor } = this.props;
         let { bootstrapped } = persistor.getState();
@@ -44,40 +43,45 @@ class App extends Component {
             <Fragment>
                 <Router history={history}>
                     <div className="main-container">
-                        <ConfirmModal />
                         {this.props.isLoggedIn && <Header />}
 
                         <span className="content-container">
                             <Switch>
-                                <Route path={path.HOME} exact component={(Home)} />
+                                <Route path={path.HOME} exact component={Home} />
                                 <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
                                 <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
+                                <Route path={path.HOMEPAGE} component={HomePage} />
                             </Switch>
                         </span>
 
                         <ToastContainer
-                            className="toast-container" toastClassName="toast-item" bodyClassName="toast-item-body"
-                            autoClose={false} hideProgressBar={true} pauseOnHover={false}
-                            pauseOnFocusLoss={true} closeOnClick={false} draggable={false}
+                            className="toast-container"
+                            toastClassName="toast-item"
+                            bodyClassName="toast-item-body"
+                            autoClose={false}
+                            hideProgressBar={true}
+                            pauseOnHover={false}
+                            pauseOnFocusLoss={true}
+                            closeOnClick={false}
+                            draggable={false}
                             closeButton={<CustomToastCloseButton />}
                         />
                     </div>
                 </Router>
             </Fragment>
-        )
+        );
     }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
         started: state.app.started,
-        isLoggedIn: state.user.isLoggedIn
+        isLoggedIn: state.user.isLoggedIn,
     };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+    return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
