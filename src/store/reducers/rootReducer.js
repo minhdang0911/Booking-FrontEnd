cf8137bd6d243310602857c29b142333d4c4d06a
Minh Dang <minhdang9a8@gmail.com>
2024-03-08 18:24:28 +0700

    Update Header

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,9 +1,9 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
-import appReducer from "./appReducer";
-import adminReducer from "./adminReducer";
-import userReducer from "./userReducer";
+import appReducer from './appReducer';
+import adminReducer from './adminReducer';
+import userReducer from './userReducer';
 
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import storage from 'redux-persist/lib/storage';
@@ -23,13 +23,13 @@ const persistCommonConfig = {
 const userPersistConfig = {
     ...persistCommonConfig,
     key: 'user',
-    whitelist: ['isLoggedIn', 'userInfo']
+    whitelist: ['isLoggedIn', 'userInfo'],
 };
 
-
-export default (history) => combineReducers({
-    router: connectRouter(history),
-    // admin: persistReducer(adminPersistConfig, adminReducer),
-    user: persistReducer(userPersistConfig, userReducer),
-    app: appReducer
-})
+export default (history) =>
+    combineReducers({
+        router: connectRouter(history),
+        // admin: persistReducer(adminPersistConfig, adminReducer),
+        user: persistReducer(userPersistConfig, userReducer),
+        app: appReducer,
+    });
