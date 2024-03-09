e575e9b87493423da52c30c7dceca9cbfa6dbc1c
Minh Dang <minhdang9a8@gmail.com>
2024-03-09 19:15:07 +0700

    Design Ui Create User

--- src/routes/System.js
+++ src/routes/System.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import UserManage from '../containers/System/UserManage';
-import UserRedux from '../containers/System/UserRedux'; // Import UserRedux instead of ProductManage
+import UserRedux from '../containers/System/Admin/UserRedux'; // Import UserRedux instead of ProductManage
 import Header from '../containers/Header/Header';
 
 class System extends Component {
