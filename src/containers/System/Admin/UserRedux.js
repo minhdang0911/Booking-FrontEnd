7c8aa0e5ad4a675a769eeac7f8fb14e24e0d0e8a
Minh Dang <minhdang9a8@gmail.com>
2024-03-10 12:48:43 +0700

    Use redux to get the list of genders to display in options

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -3,6 +3,7 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { getAllCodeService } from '../../../services/userService';
 import { LANGUAGES } from '../../../utils';
+import * as actions from '../../../store/actions';
 class UserRedux extends Component {
     constructor(props) {
         super(props);
@@ -12,21 +13,32 @@ class UserRedux extends Component {
     }
 
     async componentDidMount() {
-        try {
-            let res = await getAllCodeService('gender');
-            if (res && res.errCode === 0) {
-                this.setState({
-                    genderArr: res.data,
-                });
-            }
-        } catch (e) {
-            console.log(e);
+        this.props.getGenderStart();
+
+        // try {
+        //     let res = await getAllCodeService('gender');
+        //     if (res && res.errCode === 0) {
+        //         this.setState({
+        //             genderArr: res.data,
+        //         });
+        //     }
+        // } catch (e) {
+        //     console.log(e);
+        // }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.genderRedux !== this.props.genderRedux) {
+            this.setState({
+                genderArr: this.props.genderRedux,
+            });
         }
     }
 
     render() {
         let genders = this.state.genderArr;
         let language = this.props.language;
+        console.log('check props redux', this.props.genderRedux);
         return (
             <div className="user-redux-container">
                 <div className="title">Learn Redux</div>
@@ -139,11 +151,16 @@ class UserRedux extends Component {
 const mapStateToProps = (state) => {
     return {
         language: state.app.language,
+        genderRedux: state.admin.genders,
     };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {};
+    return {
+        getGenderStart: () => dispatch(actions.fetchGenderStart()),
+        // processLogout: () => dispatch(actions.processLogout()),
+        // changeLanguageAppRedux: (language) => dispatch(actions.changeLanguageApp(language)),
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserRedux);
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,9 +2,8 @@ import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
 import appReducer from './appReducer';
-import adminReducer from './adminReducer';
 import userReducer from './userReducer';
-
+import adminReducer from './adminReducer';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import storage from 'redux-persist/lib/storage';
 import { persistReducer } from 'redux-persist';
@@ -35,7 +34,7 @@ const appPersistConfig = {
 export default (history) =>
     combineReducers({
         router: connectRouter(history),
-        // admin: persistReducer(adminPersistConfig, adminReducer),
         user: persistReducer(userPersistConfig, userReducer),
         app: persistReducer(appPersistConfig, appReducer),
+        admin: adminReducer,
     });
